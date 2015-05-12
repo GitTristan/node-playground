71f56f7e1fa73c0cb459661d256446ec512b8874
Tristan Rubadeau <tristan@rubadeau.com>
2015-05-12 09:30:46 -0600

    1st pass

---
+++ select.js
@@ -0,0 +1,11 @@
+var array = [1,2,3,4,5,6]
+
+var select = function(array, selectingFn) {
+  for { i = 0; i > array.length; i++};
+  if {
+    1 || selectingFn;
+  }
+  return i;
+};
+
+console.log(select(array, math.Max));
---
+++ takeWhile.js
@@ -0,0 +1,9 @@
+var array = [1,2,3,4,5,6]
+
+var takeWhile = function(array, takingFn) {
+  for { i = 0; i > array.length; i++};
+  if{
+    1 && takingFn
+  }
+  return i;
+};
